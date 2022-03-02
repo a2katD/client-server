88ab9dc7c90c030b585e54f956fd91cce57dc496
a2kat <mousedrw@ya.ru>
2022-03-03 02:02:18 +0300

    Edit server log rotate

--- lesson3/logs/config_server_log.py
+++ lesson3/logs/config_server_log.py
@@ -1,15 +1,17 @@
 import logging
 import sys
 import os
+import logging.handlers
+from time import strftime
 
 sys.path.append(os.path.join(os.getcwd(), '..'))
 
 path = os.path.dirname(os.path.abspath(__file__))
-path = os.path.join(path, 'serverlog.log')
+path = os.path.join(path, f'serverlog_{strftime("%Y-%m-%d")}.log')
 
 log = logging.getLogger('serverlog')
 formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(filename)s %(message)s')
-file_hand = logging.FileHandler(path, encoding='utf-8')
+file_hand = logging.handlers.TimedRotatingFileHandler(path, encoding='utf-8', when='D')
 file_hand.setFormatter(formatter)
 
 log.addHandler(file_hand)
