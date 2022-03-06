b83237062a24dbd7be59d950dc75b901fde31a4e
a2kat <mousedrw@ya.ru>
2022-03-06 22:03:32 +0300

    lesson7

--- lesson3/client.py
+++ lesson3/client.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import sys
 import logging
@@ -10,6 +11,36 @@ from socket import socket, AF_INET, SOCK_STREAM
 from log_decor import log
 
 CLIENT_LOGGER = logging.getLogger('clientlog')
+
+
+@log
+def message_from_server(message):
+    if ACTION in message and message[ACTION] == MESSAGE and \
+            SENDER in message and MESSAGE_TEXT in message:
+        print(f'{message[SENDER]}:{message[MESSAGE_TEXT]}')
+        CLIENT_LOGGER.info(f'Получено сообщение от пользователя '
+                           f'{message[SENDER]}:\n{message[MESSAGE_TEXT]}')
+    else:
+        CLIENT_LOGGER.error(f'Получено некорректное сообщение от сервера {message}')
+
+
+@log
+def create_message(sock, account_name='Guest'):
+    message = input("Введите сообщение для отправки или "
+                    "'/quit' для завершения работы: ")
+    if message == '/quit':
+        sock.close()
+        CLIENT_LOGGER.info('Завершение работы по команде пользователя.')
+        print('Спасибо за использование нашего сервиса!')
+        sys.exit(0)
+    message_dict = {
+        ACTION: MESSAGE,
+        TIME: time(),
+        ACCOUNT_NAME: account_name,
+        MESSAGE_TEXT: message
+    }
+    CLIENT_LOGGER.debug(f'Сформирован словарь сообщения: {message_dict}')
+    return message_dict
 
 
 @log
@@ -33,26 +64,35 @@ def process_ans(meassage):
             return "200: OK"
         return f'{meassage[RESPONSE]}: {meassage[ERROR]}'
     CLIENT_LOGGER.error(f"Сообщение не содержит {RESPONSE}")
-    raise ValueError
+    raise ReqFieldMissingError(RESPONSE)
+
+
+@log
+def arg_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('addr', default=DEFAULT_ADDR, nargs='?')
+    parser.add_argument('port', default=DEFAULT_PORT, type=int, nargs='?')
+    parser.add_argument('-m', '--mode', default='listen', nargs='?')
+    namespace = parser.parse_args(sys.argv[1:])
+    server_address = namespace.addr
+    server_port = namespace.port
+    client_mode = namespace.mode
+
+    if not 1023 < server_port < 65536:
+        CLIENT_LOGGER.critical(
+            f'Попытка запуска клиента с указанием неподоходящего порта {server_port}'
+            f'Доступные порты с 1024 по 65535')
+        sys.exit(1)
+
+    if client_mode not in ('listen', 'send'):
+        CLIENT_LOGGER.critical(f'Указан недопустимый режим работы {client_mode}, '
+                               f'допустимые режимы: listen , send')
+        sys.exit(1)
+    return server_address, server_port, client_mode
 
 
 def main():
-    try:
-        if len(sys.argv) > 1:
-            server_address = sys.argv[1]
-        else:
-            server_address = DEFAULT_ADDR
-        if len(sys.argv) > 2:
-            server_port = int(sys.argv[2])
-            if 1024 > server_port < 65535:
-                CLIENT_LOGGER.error(
-                    f'Попытка запуска клиента с указанием неподоходящего порта {server_port}. Доступные порты с 1024 по 65535')
-                raise ValueError
-        else:
-            server_port = DEFAULT_PORT
-    except ValueError:
-        CLIENT_LOGGER.error("Неверно указан порт")
-        sys.exit(1)
+    server_address, server_port, client_mode = arg_parser()
 
     CLIENT_LOGGER.info(f"Запущен клиент с параметрами:"
                        f'адрес сервера:{server_address}, порт: {server_port}')
@@ -63,15 +103,41 @@ def main():
         massage_to_server = create_presence()
         send_message(transport, massage_to_server)
         answer = process_ans(get_message(transport))
-        print(answer)
+        CLIENT_LOGGER.info(f'Установлено соединение с сервером. Ответ сервера: {answer}')
+        print(f'Установлено соединение с сервером.')
     except json.JSONDecodeError:
         CLIENT_LOGGER.error(f'Не удалось декодировать Json строку')
+        sys.exit(1)
     except ReqFieldMissingError as missing_error:
         CLIENT_LOGGER.error(f'В ответе сервера отсутствует необходимое поле'
                             f'{missing_error.missing_field}')
+        sys.exit(1)
     except ConnectionRefusedError:
-        CLIENT_LOGGER.critical(f'Не удалось подключиться к серверу {server_address}:{server_port}'
-                               f'Сервер отверг запрос на подключение')
+        CLIENT_LOGGER.critical(
+            f'Не удалось подключиться к серверу {server_address}:{server_port}'
+            f'Сервер отверг запрос на подключение')
+        sys.exit(1)
+    except ServerError as error:
+        CLIENT_LOGGER.error(f'При установке соединения сервер вернул ошибку: {error.text}')
+        sys.exit(1)
+    else:
+        if client_mode == 'send':
+            print('Режим работы - отправка сообщений.')
+        else:
+            print('Режим работы - приём сообщений.')
+        while True:
+            if client_mode == 'send':
+                try:
+                    send_message(transport, create_message(transport))
+                except (ConnectionResetError, ConnectionError, ConnectionAbortedError):
+                    CLIENT_LOGGER.error(f'Соединение с сервером {server_address} было потеряно.')
+                    sys.exit(1)
+            if client_mode == 'listen':
+                try:
+                    message_from_server(get_message(transport))
+                except (ConnectionResetError, ConnectionError, ConnectionAbortedError):
+                    CLIENT_LOGGER.error(f'Соединение с сервером {server_address} было потеряно.')
+                    sys.exit(1)
 
 
 if __name__ == '__main__':
--- lesson3/common/variables.py
+++ lesson3/common/variables.py
@@ -8,8 +8,14 @@ ACTION = 'action'
 TIME = 'time'
 USER = 'user'
 ACCOUNT_NAME = 'account_name'
+SENDER = 'sender'
 
 PRESENCE = 'presence'
 RESPONSE = 'response'
 ERROR = 'error'
 RESPONDEFAULT_IP_ADDRESSSE = 'respondefault_ip_adressse'
+MESSAGE = 'message'
+MESSAGE_TEXT = 'mess_text'
+
+CLIENTS_LISTEN_COUNT = 2
+CLIENTS_SENDER_COUNT = 2
