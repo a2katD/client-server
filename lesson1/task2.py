c32d3c067a9a77dd190705aa5e4bb767b90e683a
a2kat <mousedrw@ya.ru>
2022-03-30 22:24:16 +0300

    task2

--- lesson1/task2.py
+++ lesson1/task2.py
@@ -1,6 +1,25 @@
+import socket
 from task1 import host_ping
+from ipaddress import ip_address
 
 
-def host_range_ping(ip_list, ip_range):
-    for i in ip_range:
-        host_ping
+def host_range_ping():
+    while True:
+        ip = input('Введите начальный ip адрес: ')
+        try:
+            IPV4 = ip_address(socket.gethostbyname(ip))
+        except:
+            print('Некорректный ip адрес')
+            continue
+        quantity = input('Введите сколько адресов проверить: ')
+        if not quantity.isdigit() or int(quantity) <= 0:
+            print('Неверное значение, введите положительное число')
+            continue
+        break
+
+    for i in range(int(quantity)):
+        host_ping([IPV4 + i])
+
+
+if __name__ == '__main__':
+    host_range_ping()
